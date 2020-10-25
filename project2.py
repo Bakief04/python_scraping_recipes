07c063502ebb00b1ff9e7380e20060a73392fd49
Brian Kiefer <bakief04@gmail.com>
2020-10-25 15:31:32 -0400

    fiugred out how to grab first 3 results from search

--- project2.py
+++ project2.py
@@ -1,9 +1,13 @@
 from bs4 import BeautifulSoup
+from urllib.request import urlopen as url
 import requests
 
-user_input = input("What recipe are you looking for?" )
+user_input = input("What recipe are you looking for? " )
 source = requests.get ('https://www.allrecipes.com/search/?wt={}'.format(user_input)).text
 
 soup = BeautifulSoup(source, 'html.parser')
 
-print (soup.prettify())
+#Sarticle = soup.find(id='fixed-recipe-card')
+#print(article.prettify())
+summary = soup.find_all('div', class_='grid-card-image-container', limit=3)
+print(summary)
