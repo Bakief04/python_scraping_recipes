6e3664c6c1dcf4bfcaef9dfd36b71f7180ecf0b6
Brian Kiefer <bakief04@gmail.com>
2020-10-31 02:06:49 -0400

    dded string replace

--- project2.py
+++ project2.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen as url
 import requests
 import sys
-
+import string
 
 user_input = input("What recipe are you looking for? " )
 source = requests.get ('https://www.allrecipes.com/search/?wt={}'.format(user_input)).text
@@ -11,14 +11,15 @@ soup = BeautifulSoup(source, 'html.parser')
 
 #prints out the recipe titles and stores the url for the recipe as a variable
 for recipe_title in soup.find_all('a',{'class':'fixed-recipe-card__title-link'}, limit=3):
-    py1 = (recipe_title['href'])
     print(recipe_title.get_text())
     
 py3 = input("Please type the recipe which closely matches the recipe that you would like. ")
-py2 = source = requests.get ("https://www.allrecipes.com/recipe/{}".format(py3)).text #url of first site url in recipe_title
-py4 = input("Are you sure this is correct? [Y/N]  ") 
+py3 = py3.replace(' ', '-')
+#url of first site url in recipe_title
+py2 = source = requests.get ("https://www.allrecipes.com/recipe/{}".format(py3)).text
+py4 = input("Are you sure this is correct? ({}) [Y/N]  ".format(py3.replace('-', ' ')))
 
-#writes to file. Currently writes the full html if you type 1 at the end
+#writes to file. Currently writes the full html
 if py4 == 'Y':
-    sys.stdout = open('{}.txt'.format(py3), 'wt')
+    sys.stdout = open('{}.txt'.format(py3.replace('-', ' ')), 'wt')
     print(py2)
