2480a993c8c19aac6d37f87171d6888a7188a5ed
Brian Kiefer <bakief04@gmail.com>
2020-10-31 02:18:40 -0400

    spacing and cleaning

--- project2.py
+++ project2.py
@@ -10,16 +10,20 @@ source = requests.get ('https://www.allrecipes.com/search/?wt={}'.format(user_in
 soup = BeautifulSoup(source, 'html.parser')
 
 #prints out the recipe titles and stores the url for the recipe as a variable
+
 for recipe_title in soup.find_all('a',{'class':'fixed-recipe-card__title-link'}, limit=3):
     print(recipe_title.get_text())
     
 py3 = input("Please type the recipe which closely matches the recipe that you would like. ")
 py3 = py3.replace(' ', '-')
+
 #url of first site url in recipe_title
+
 py2 = source = requests.get ("https://www.allrecipes.com/recipe/{}".format(py3)).text
 py4 = input("Are you sure this is correct? ({}) [Y/N]  ".format(py3.replace('-', ' ')))
 
 #writes to file. Currently writes the full html
+
 if py4 == 'Y':
     sys.stdout = open('{}.txt'.format(py3.replace('-', ' ')), 'wt')
     print(py2)
