451849f4ebdde3455449e87c4d1198c2f21fb670
Brian Kiefer <bakief04@gmail.com>
2020-11-04 17:00:05 -0500

    fixed issue with javascript

---
+++ README.md
@@ -0,0 +1,5 @@
+This is my python priject. It is made to let you search for a recipe and choose from a list that is generated and export the recipe ingredients and directions.
+The requirements that are being fulfilled are using web scraping, writing to a file, converting json to a dictionary, and getting values from that dictionary.
+
+Code need beautifulsoup to work. You can do so by using the command pip install beautifulsoup4.
+Code needs requests to work. You can do so by using the command pip install requests
--- project2.py
+++ project2.py
@@ -1,9 +1,7 @@
-from bs4 import BeautifulSoup
-from urllib.request import urlopen as url
 import requests
-import sys
-import string
 import json
+from bs4 import BeautifulSoup
+import sys
 
 user_input = input("What recipe are you looking for? " )
 source = requests.get ('https://www.allrecipes.com/search/?wt={}'.format(user_input)).text
@@ -11,24 +9,31 @@ source = requests.get ('https://www.allrecipes.com/search/?wt={}'.format(user_in
 soup = BeautifulSoup(source, 'html.parser')
 
 #prints out the recipe titles and stores the url for the recipe as a variable
-
 for recipe_title in soup.find_all('a',{'class':'fixed-recipe-card__title-link'}, limit=3):
     print(recipe_title.get_text())
-    
-py3 = input("Please type the recipe which closely matches the recipe that you would like. ")
-py3 = py3.replace(' ', '-')
 
-#url of first site url in recipe_title
+py3 = input("What recipe are you looking for? ")
+py3 = py3 = py3.replace(' ', '-')
+url = 'https://www.allrecipes.com/recipe/{}'.format(py3)
+#url for recipe with variable passed in
 
-py2 = source = requests.get ("https://www.allrecipes.com/recipe/{}".format(py3)).text
-soup2 = soup.find_all({'ul class':'ingredients-section'})
-for ingredients in soup.find_all('ul', {'class': 'recipeIngredient'}):
-    print(ingredients.get_text())
+r = requests.get(url) 
+soup = BeautifulSoup(r.content, "html.parser")
 
-py4 = input("Are you sure this is correct? ({}) [Y/N]  ".format(py3.replace('-', ' ')))
+#parses json to treat variable as dictionary
 
-#writes to file. Currently writes the full html
+start = '<script type="application/ld+json">'
+end = '</script>'
+json_data = r.text[r.text.find(start) + len(start):r.text.find(end)]
+data = json.loads(json_data)[1]
 
-if py4 == 'Y':
-    sys.stdout = open('{}.txt'.format(py3.replace('-', ' ')), 'wt')
-    print(py2)
+#Writes to file
+
+sys.stdout = open('{}.txt'.format(py3.replace('-', ' ')), 'w+')
+print('Ingredients')
+for person in data['recipeIngredient']:
+	print(person)
+print('\n')
+print('Instructions')
+for step in data["recipeInstructions"]:
+	print(step['text'])
