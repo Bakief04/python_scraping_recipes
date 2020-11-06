9f8b6ebb248b693617984c61d36a11282e4edfce
Brian Kiefer <bakief04@gmail.com>
2020-11-05 23:48:30 -0500

    Fixed infinite loop and spacing

--- project2.py
+++ project2.py
@@ -3,40 +3,52 @@ import json
 from bs4 import BeautifulSoup
 import sys
 
-user_input = input("What recipe are you looking for? (type quit to exit) " )
-
-source = requests.get ('https://www.allrecipes.com/search/?wt={}'.format(user_input)).text
-soup = BeautifulSoup(source, 'html.parser')
+while True:
+    quit_program = 'quit'
+    user_input = input("What recipe are you looking for? (type quit to exit) " )
+    if user_input == quit_program:
+        sys.exit("thank you for using this program")
+    source = requests.get ('https://www.allrecipes.com/search/?wt={}'.format(user_input)).text
+    soup = BeautifulSoup(source, 'html.parser')
 
 #prints out the recipe titles and stores the url for the recipe as a variable
-for recipe_title in soup.find_all('a',{'class':'fixed-recipe-card__title-link'}, limit=3):
-    print(recipe_title.get_text())
+    for recipe_title in soup.find_all('a',{'class':'fixed-recipe-card__title-link'}, limit=3):
+        print(recipe_title.get_text())
 
-py3 = input("What recipe best matches what you are looking for? ")
-py3 = py3 = py3.replace(' ', '-')
-url = 'https://www.allrecipes.com/recipe/{}'.format(py3)
-
+    py3 = input("What recipe best matches what you are looking for? ")
+    py3 = py3.replace(' ', '-')
+    url = 'https://www.allrecipes.com/recipe/{}'.format(py3)
+    if py3 == quit_program:
+        sys.exit('thank you for using the program')
 # url for recipe with variable passed in
 
-r = requests.get(url) 
-soup = BeautifulSoup(r.content, "html.parser")
+    r = requests.get(url) 
+    soup = BeautifulSoup(r.content, "html.parser")
 
 #parses json to treat variable as dictionary
+    start = '<script type="application/ld+json">'
+    end = '</script>'
+    json_data = r.text[r.text.find(start) + len(start):r.text.find(end)]
+    data = json.loads(json_data)[1]
 
-start = '<script type="application/ld+json">'
-end = '</script>'
-json_data = r.text[r.text.find(start) + len(start):r.text.find(end)]
-data = json.loads(json_data)[1]
-
-confirm = input('Are you sure this is correct?[Y/N] ')
-print('Thank you, The recipe is in a file for easier viewing')
+    confirm = input('Are you sure this is correct?[Y/N] ')
+    print('Thank you, The recipe is in a file for easier viewing')
 #Writes to file
-if confirm == ('Y'):
-    test = sys.stdout = open('{}.txt'.format(py3.replace('-', ' ')), 'w+')
-    print('Ingredients')
-    for person in data['recipeIngredient']:
-        print(person)
-    print('\n')
-    print('Instructions')
-    for step in data["recipeInstructions"]:
-        print(step['text'])
+    if confirm == ('Y'):
+        with open('{}.txt'.format(py3.replace('-', ' ')), 'w+') as file1:
+            file1.write('Ingredients')
+            file1.write('\n')
+            for person in data["recipeIngredient"]:
+	            file1.writelines(person + '\n')
+            file1.write('\n')
+            file1.write('Instructions')
+            file1.write('\n')
+            for step in data["recipeInstructions"]:
+	            file1.writelines(step['text'])
+        
+        
+        exit_program = input("Would you like to find another recipe? [Y/N] ")
+        if exit_program == "N":
+            sys.exit('thank you for using the program')
+    else: 
+        continue
