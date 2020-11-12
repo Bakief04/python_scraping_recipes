9044fbe3ca5fe54694877501ee3321f23e1ce506
Brian Kiefer <bakief04@gmail.com>
2020-11-12 13:09:17 -0500

    added to readme and file

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ The requirements that are being fulfilled are using web scraping, infinite loop,
 
 Code need beautifulsoup to work. You can do so by using the command pip install beautifulsoup4.
 Code needs requests to work. You can do so by using the command pip install requests
+
+I've encountered some recipes don't process JSON properly. If thats the case try another recipe
--- project2.py
+++ project2.py
@@ -9,12 +9,11 @@ while True:
     if user_input == quit_program:
         sys.exit("thank you for using this program")
     source = requests.get ('https://www.allrecipes.com/search/?wt={}'.format(user_input)).text
-    soup = BeautifulSoup(source, 'html.parser')
+    soup = BeautifulSoup(source, 'lxml')
 
 #prints out the recipe titles and stores the url for the recipe as a variable
     for recipe_title in soup.find_all('a',{'class':'fixed-recipe-card__title-link'}, limit=3):
         print(recipe_title.get_text())
-
     py3 = input("What recipe best matches what you are looking for? ")
     py3 = py3.replace(' ', '-')
     url = 'https://www.allrecipes.com/recipe/{}'.format(py3)
@@ -23,32 +22,41 @@ while True:
 # url for recipe with variable passed in
 
     r = requests.get(url) 
-    soup = BeautifulSoup(r.content, "html.parser")
+    soup = BeautifulSoup(r.content, "lxml")
 
 #parses json to treat variable as dictionary
     start = '<script type="application/ld+json">'
     end = '</script>'
-    json_data = r.text[r.text.find(start) + len(start):r.text.find(end)]
-    data = json.loads(json_data)[1]
-
+    try:
+        json_data = r.text[r.text.find(start) + len(start):r.text.find(end)]
+        data = json.loads(json_data)[1]
+    except json.decoder.JSONDecodeError:
+        print("Poorly formatted JSON, please try another recipe")
+        break
     confirm = input('Are you sure this is correct?[Y/N] ')
-    print('Thank you, The recipe is in a file for easier viewing')
 #Writes to file
-    if confirm == ('Y'):
-        with open('{}.txt'.format(py3.replace('-', ' ')), 'w+') as file1:
-            file1.write('Ingredients')
-            file1.write('\n')
-            for person in data["recipeIngredient"]:
-	            file1.writelines(person + '\n')
-            file1.write('\n')
-            file1.write('Instructions')
-            file1.write('\n')
-            for step in data["recipeInstructions"]:
-	            file1.writelines(step['text'])
-        
-        
-        exit_program = input("Would you like to find another recipe? [Y/N] ")
-        if exit_program == "N":
-            sys.exit('thank you for using the program')
+    try:
+        if confirm == ('Y'):
+            print('Thank you, The recipe is in a file for easier viewing')
+            with open('{}.txt'.format(py3.replace('-', ' ')), 'w+') as file1:
+                file1.write('Ingredients')
+                file1.write('\n')
+                for person in data["recipeIngredient"]:
+	                file1.writelines(person + '\n')
+                file1.write('\n')
+                file1.write('Instructions')
+                file1.write('\n')
+                for step in data["recipeInstructions"]:
+	                file1.writelines(step['text'])
+        elif confirm == ('N'):
+            print('Run Program to Try Again')
+            break
+    except UnicodeEncodeError:
+        print("Poorly formatted JSON, please try another recipe")
+        break
+            
+    exit_program = input("Would you like to find another recipe? [Y/N] ")
+    if exit_program == "N":
+        sys.exit('thank you for using the program')
     else: 
         continue
